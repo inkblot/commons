4b65188301ffad9fdff83cb8d34f2d06fa5da412
inkblot <inkblot@99c579c4-a562-4650-8cc4-2e4534c67d48>
2010-08-21 09:25:09 +0000

    Add find and apply.  Create a Test implementation that wraps a hamcrest Matcher and a method (matches) that does the decoration, and create delegates of detect, find, and filter that take a hamcrest Matcher.  Implement a reverse method in terms of foldRight.  Test everything.
    git-svn-id: https://secure.movealong.org/svn/trunk/commons@282 99c579c4-a562-4650-8cc4-2e4534c67d48

--- src/org/movealong/common/util/Functional.java
+++ src/org/movealong/common/util/Functional.java
@@ -1,52 +1,34 @@
 package org.movealong.common.util;
 
+import org.hamcrest.Matcher;
+
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 public class Functional {
 
-	private Functional() {}
+    private Functional() {}
 
-	public static <InputType, AccumulatorType> AccumulatorType foldRight(List<InputType> input, Folder<InputType, AccumulatorType> folder, AccumulatorType seed) {
-		if (input.isEmpty()) {
-			return seed;
-		} else {
-			return foldRight(input.subList(1, input.size()), folder, folder.fold(input.get(0), seed));
-		}
-	}
-
-	public static <InputType, AccumulatorType> AccumulatorType foldLeft(List<InputType> input, Folder<InputType, AccumulatorType> folder, AccumulatorType seed) {
-		if (input.isEmpty()) {
-			return seed;
-		} else {
-			return foldLeft(input.subList(0, input.size() - 1), folder, folder.fold(input.get(input.size() - 1), seed));
-		}
-	}
-
-    public static <E> boolean detect(Collection<E> elements, Test<E> test) {
+    public static <E> void apply(Collection<E> elements, Lambda<E> lambda) {
         for (E element : elements) {
-            if (test.test(element)) return true;
+            lambda.apply(element);
         }
-        return false;
     }
 
-    public static interface Folder<InputType, AccumulatorType> {
-		AccumulatorType fold(InputType input, AccumulatorType accumulator);
-	}
+    public static <E> boolean detect(Collection<E> elements, Matcher<E> matcher) {
+        return detect(elements, matches(matcher));
+    }
 
-	public static <InputType, OutputType, InputCollection extends Collection<InputType>, OutputCollection extends Collection<OutputType>> OutputCollection map(InputCollection source, Mapper<InputType,OutputType> mapper) {
-		OutputCollection target = (OutputCollection) CollectionUtil.emptyCopy(source);
-		for (InputType element : source) {
-			target.add(mapper.map(element));
-		}
-		return target;
-	}
+    public static <E> boolean detect(Collection<E> elements, Test<E> test) {
+        return find(elements, test) != null;
+    }
 
-	public static interface Mapper<InputType, OutputType> {
-		OutputType map(InputType source);
-	}
+    public static <InputCollection extends Collection<E>, OutputCollection extends Collection<E>, E> OutputCollection filter(InputCollection unfiltered, Matcher<E> test, OutputCollection accumulator) {
+        return filter(unfiltered, matches(test), accumulator);
+    }
 
-    public static <InputCollection extends Collection<E>, OutputCollection extends Collection<E>, E> Collection filter(InputCollection unfiltered, Test<E> test, OutputCollection accumulator) {
+    public static <InputCollection extends Collection<E>, OutputCollection extends Collection<E>, E> OutputCollection filter(InputCollection unfiltered, Test<E> test, OutputCollection accumulator) {
         for (E element : unfiltered) {
             if (test.test(element)) {
                 accumulator.add(element);
@@ -55,7 +37,98 @@ public class Functional {
         return accumulator;
     }
 
+    public static <E> E find(Collection<E> elements, Matcher<E> matcher) {
+        return find(elements, matches(matcher));
+    }
+
+    public static <E> E find(Collection<E> elements, Test<E> test) {
+        for (E element : elements) {
+            if (test.test(element)) return element;
+        }
+        return null;
+    }
+
+    public static <InputType, AccumulatorType> AccumulatorType foldLeft(List<InputType> input, Folder<InputType, AccumulatorType> folder) {
+        return foldLeft(input, folder, null);
+    }
+
+    public static <InputType, AccumulatorType> AccumulatorType foldLeft(List<InputType> input, Folder<InputType, AccumulatorType> folder, AccumulatorType seed) {
+        AccumulatorType accumulator = seed;
+        for (int index = 0; index < input.size(); index++) {
+            accumulator = folder.fold(input.get(index), accumulator);
+        }
+        return accumulator;
+    }
+
+    public static <InputType, AccumulatorType> AccumulatorType foldRight(List<InputType> input, Folder<InputType, AccumulatorType> folder) {
+        return foldRight(input, folder, null);
+    }
+
+    public static <InputType, AccumulatorType> AccumulatorType foldRight(List<InputType> input, Folder<InputType, AccumulatorType> folder, AccumulatorType seed) {
+        AccumulatorType accumulator = seed;
+        for (int index = input.size() - 1; index >= 0; index--) {
+            accumulator = folder.fold(input.get(index), accumulator);
+        }
+        return accumulator;
+    }
+
+    public static <InputType, OutputType, InputCollection extends Collection<InputType>, OutputCollection extends Collection<OutputType>> OutputCollection map(InputCollection source, Mapper<InputType, OutputType> mapper) {
+        OutputCollection target = (OutputCollection) CollectionUtil.emptyCopy(source);
+        return map(source, mapper, target);
+    }
+
+    public static <InputType, OutputType, InputCollection extends Collection<InputType>, OutputCollection extends Collection<OutputType>> OutputCollection map(InputCollection source, Mapper<InputType, OutputType> mapper, OutputCollection target) {
+        for (InputType element : source) {
+            target.add(mapper.map(element));
+        }
+        return target;
+    }
+
+    public static <T> Test<T> matches(Matcher<T> matcher) {
+        return new MatcherTest<T>(matcher);
+    }
+
+    public static <E> List<E> reverse(List<E> elements) {
+        return foldRight(
+                elements,
+                new Folder<E, List<E>>() {
+                    @Override
+                    public List<E> fold(E input, List<E> accumulator) {
+                        accumulator.add(input);
+                        return accumulator;
+                    }
+                },
+                new ArrayList<E>());
+    }
+
+    public static interface Folder<InputType, AccumulatorType> {
+        AccumulatorType fold(InputType input, AccumulatorType accumulator);
+    }
+
+    public static interface Lambda<T> {
+        void apply(T item);
+    }
+
+    public static interface Mapper<InputType, OutputType> {
+        OutputType map(InputType source);
+    }
+
+    private static class MatcherTest<T> implements Test<T> {
+
+        private final Matcher<T> matcher;
+
+        public MatcherTest(Matcher<T> matcher) {
+            this.matcher = matcher;
+        }
+
+        @Override
+        public boolean test(T element) {
+            return matcher.matches(element);
+        }
+    }
+
     public static interface Test<E> {
         boolean test(E element);
     }
+
 }
--- test/org/movealong/common/util/FunctionalTest.java
+++ test/org/movealong/common/util/FunctionalTest.java
@@ -2,39 +2,115 @@ package org.movealong.common.util;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-import junit.framework.JUnit4TestAdapter;
-
 import org.junit.Test;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
+import static org.movealong.common.util.Functional.*;
 
 public class FunctionalTest {
 
-	@Test
-	public void mapList() {
-		List<Integer> inputList = Arrays.<Integer>asList(1, 2, 3, 4, 5);
-		List<Number> outputList = Functional.map(inputList,
-				new Functional.Mapper<Integer, Number>() {
-					public Number map(Integer i) {
-						return new BigDecimal(i);
-					}
-				});
-		assertArrayEquals(
-				new BigDecimal[] {
-					new BigDecimal(1),
-					new BigDecimal(2),
-					new BigDecimal(3),
-					new BigDecimal(4),
-					new BigDecimal(5)
-				},
-				outputList.toArray(new BigDecimal[0]));
-		assertEquals(ArrayList.class, outputList.getClass());
-	}
+    @Test
+    public void _apply() throws Exception {
+        final Object alpha = new Object();
+        final Object beta = new Object();
+        final Object gamma = new Object();
+        final Object delta = new Object();
+        final List<Object> visited = new ArrayList<Object>();
+        apply(
+                asList(alpha, beta, gamma, delta),
+                new Functional.Lambda<Object>() {
+                    @Override
+                    public void apply(Object item) {
+                        visited.add(item);
+                    }
+                });
+        assertThat(visited, equalTo(asList(alpha, beta, gamma, delta)));
+    }
 
-	public static junit.framework.Test suite() {
-		return new JUnit4TestAdapter(FunctionalTest.class);
-	}
+    @Test
+    public void _detect() throws Exception {
+        final Object alpha = new Object();
+        final Object beta = new Object();
+        final Object gamma = new Object();
+        final Object delta = new Object();
+        final List<Object> objects = asList(alpha, beta, delta);
+        assertTrue(detect(objects, sameInstance(beta)));
+        assertFalse(detect(objects, sameInstance(gamma)));
+    }
+
+    @Test
+    public void _filter() throws Exception {
+        assertThat(
+                filter(
+                        asList(1, 2, 3, 4, 5),
+                        lessThan(3),
+                        new ArrayList<Integer>()),
+                equalTo(asList(1, 2)));
+    }
+
+    @Test
+    public void _find() throws Exception {
+        final Object alpha = new Object();
+        final Object beta = new Object();
+        final Object gamma = new Object();
+        final Object delta = new Object();
+        final List<Object> objects = asList(alpha, beta, delta);
+        assertThat(
+                find(
+                        objects,
+                        sameInstance(beta)),
+                sameInstance(beta));
+        assertThat(
+                find(
+                        objects,
+                        sameInstance(gamma)),
+                nullValue());
+    }
+
+    @Test
+    public void _fold() throws Exception {
+        Folder<Integer, List<Integer>> appender =
+                new Folder<Integer, List<Integer>>() {
+                    @Override
+                    public List<Integer> fold(Integer input, List<Integer> accumulator) {
+                        List<Integer> list = accumulator == null ? new ArrayList<Integer>() : accumulator;
+                        list.add(input);
+                        return list;
+                    }
+                };
+
+        assertThat(
+                foldRight(asList(1, 2, 3, 4, 5), appender),
+                equalTo(asList(5, 4, 3, 2, 1)));
+        assertThat(
+                foldLeft(asList(1, 2, 3, 4, 5), appender),
+                equalTo(asList(1, 2, 3, 4, 5)));
+        assertThat(
+                reverse(asList(1, 2, 3, 4, 5)),
+                equalTo(asList(5, 4, 3, 2, 1)));
+    }
+
+    @Test
+    public void _map() throws Exception {
+        assertThat(
+                map(
+                        asList(1, 2, 3, 4, 5),
+                        new Functional.Mapper<Integer, BigDecimal>() {
+                            public BigDecimal map(Integer i) {
+                                return new BigDecimal(i);
+                            }
+                        },
+                        new ArrayList<BigDecimal>()),
+                equalTo(asList(
+                        new BigDecimal(1),
+                        new BigDecimal(2),
+                        new BigDecimal(3),
+                        new BigDecimal(4),
+                        new BigDecimal(5))));
+    }
 
 }
